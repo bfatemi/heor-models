e13bc808a621b40c90b41197e54e47756ab60d14
Bobby Fatemi <Bobby.Fatemi@intusurg.com>
2017-07-20 09:52:24 -0700

    tests ok. Changed dependency to cran rlang instead of dev version.

--- tests/testthat/test-basic.R
+++ tests/testthat/test-basic.R
@@ -1,11 +1,14 @@
 
 
 test_that("Running a diff of means between psm matched groups", {
-
+  print(getwd())
   skip_on_travis()
+  skip_on_cran()
   
-  Sys.setenv(USER_KEY = "~/.ssh/private.pk")
-  
+  op <- getOption("secret.vault")
+  options(secret.vault = "C:/Users/bobbyf/Documents/isds/inst/vault")
+  on.exit(options(secret.vault = op))
+
   DT <- getDataQTI()
   expect_true(is.data.table(DT))  
   expect_true(nrow(DT) > 1)
@@ -13,18 +16,18 @@ test_that("Running a diff of means between psm matched groups", {
   strat_vars   <- c("PRIMARY_PROCEDURE", "BENIGN_MALIGNANT", "PATIENT_TYPE")
   covariates   <- c("BMI", "PATIENT_AGE", "CHARLSON_SCORE", "PATIENT_GENDER")
   outcome_vars <- c("LOS_HOURS", "OR_TIME_MINS")
-  
-  psm_dt <- psm_data(DT = DT, 
-                       strat_vars = strat_vars, 
-                       covariates = covariates, 
+
+  psm_dt <- psm_data(DT = DT,
+                       strat_vars = strat_vars,
+                       covariates = covariates,
                        outcome_vars = outcome_vars)
-  expect_true(is.data.table(psm_dt))  
+  expect_true(is.data.table(psm_dt))
   expect_true(nrow(psm_dt) > 1)
-  
+
   sdt <- get_stats(psm_dt, outcome_vars)
-  expect_true(is.data.table(sdt))  
+  expect_true(is.data.table(sdt))
   expect_true(nrow(sdt) > 1)
-  
+
   runPSM(hosp_id = 10112)
 })
 
